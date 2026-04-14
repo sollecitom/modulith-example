5379ccc69c35bb32879156235b7c0321322c9801
Michele Sollecito <msollecito@meta.com>
2026-04-14 16:36:29 +0100

    WIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,4 @@ plugins {
 val versionDetails: Closure<VersionDetails> by extra
 extra["gitVersion"] = versionDetails()
 extra["dockerRemoteRepository"] = findProperty("dockerRemoteRepository") ?: "ghcr.io/sollecitom"
-extra["dockerBaseImageParam"] = findProperty("dockerBaseImageParam") ?: "eclipse-temurin:25-alpine"
+extra["dockerBaseImageParam"] = findProperty("dockerBaseImageParam") ?: "eclipse-temurin:25-jre-noble"
