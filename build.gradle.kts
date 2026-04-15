9c18b90a6160b5085019efbc1eb529f0ae813de5
Michele Sollecito <msollecito@meta.com>
2026-04-15 13:52:15 +0100

    WIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,12 @@ import groovy.lang.Closure
 
 plugins {
     alias(libs.plugins.com.palantir.git.version)
-    id("sollecitom.dependency-update-conventions")
-    id("sollecitom.aggregate-test-metrics-conventions")
+    alias(libs.plugins.sollecitom.dependency.update.conventions)
+    alias(libs.plugins.sollecitom.aggregate.test.metrics.conventions)
+    alias(libs.plugins.sollecitom.kotlin.library.conventions) apply false
+    alias(libs.plugins.sollecitom.jib.docker.build.conventions) apply false
+    alias(libs.plugins.sollecitom.container.based.service.test.conventions) apply false
+    alias(libs.plugins.sollecitom.security.scan.conventions) apply false
 }
 
 val versionDetails: Closure<VersionDetails> by extra
